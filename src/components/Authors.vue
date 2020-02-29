b5234a80cee3b53ed7d5ae46f7fdff5042093acf
isikli <isiklia@gmail.com>
2020-03-01 09:01:50 +1100

    bug fixes for authors panel

--- src/components/Authors.vue
+++ src/components/Authors.vue
@@ -1,8 +1,8 @@
 <template>
 <div id="authors">
 <div class="formflds">
-<label>First Name</label><input type="text" v-model="newAuthor.firstName" autofocus/>
-<label>Last Name</label><input type="text" v-model="newAuthor.lastName"/>
+<label>First Name</label><input v-debounce:300ms="getAuthors" type="text" v-model="newAuthor.firstName" autofocus/>
+<label>Last Name</label><input v-debounce:300ms="getAuthors" type="text" v-model="newAuthor.lastName"/>
 <label>Create</label><button v-on:click="createAuthor">&#43;</button>
 </div>
 
@@ -19,7 +19,6 @@
 import axios from 'axios';
 import ActionResultPane from './ActionResultPane.vue'
 import ListAuthors from './ListAuthors.vue'
-
 
 export default {
 components: {
@@ -50,6 +49,28 @@ currentObj.reqResult = error;
 
 	methods: {
 
+  getAuthors: function ()
+  {
+    let currentObj = this;
+    console.log ("getAuthors")
+    axios.get(this.AUTHORS_URL+'?firstName='+currentObj.newAuthor.firstName+'&lastName='+currentObj.newAuthor.lastName)
+    .then(
+    function (response)
+    {
+     console.log (response);
+      if (response)
+      {
+        currentObj.authors = response.data;
+      } else
+      {
+        currentObj.authors = [];
+      }
+    }).catch(function (error) {
+    console.log (error);
+    currentObj.reqResult = error.response;});
+    console.log (this.authors);
+  },
+
     load: function ()
     {
       var currentObj = this;
@@ -69,8 +90,15 @@ currentObj.reqResult = error;
         lastName: this.newAuthor.lastName
         })
       .then( function (response) {
+      console.log (response.data.id);
         currentObj.reqResult = response;
-        currentObj.newAuthor.firstName = currentObj.newAuthor.lastName = '';
+        var createdAuthor;
+        createdAuthor.authors.id = response.data.id;
+        createdAuthor.authors.firstName = response.data.firstName;
+        createdAuthor.authors.lastName = response.data.lastName;
+        currentObj.authors.push (createdAuthor);
+
+      //  currentObj.newAuthor.firstName = currentObj.newAuthor.lastName = '';
         }).catch(function (error) {
     // handle error
     currentObj.reqResult = error;
